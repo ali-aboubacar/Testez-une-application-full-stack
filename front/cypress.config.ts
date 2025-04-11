8518753d287518bd6a4ef9bb7a07e03a080f001c
Aboubacar <ali.02.aboubacar@gmail.com>
2025-04-11 15:26:50 +0200

    ignore nyc_output

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -8,9 +8,11 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
+    experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.ts').default(on, config)
     },
     baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/*.cy.ts',
   },
 })
